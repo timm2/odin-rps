42724336eacd886834d8c2b90a6599f5fbe396c2
timm2 <timlmoran424@gmail.com>
2022-05-30 18:10:32 -0500

    Added pseudocode for script

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 /*
 Inputs: Player's selection
 Outputs: who wins the game
+
+Create function called computerPlay
+    Generate a random number from 1-3
+    Each number represents rock, paper, or scissors
+    Return the string the number chose
+
+Create function called playRound
+    It takes two parameters: playerSelection and computerSelection
+    Compare the values and return if the player wins or loses
+
+Create a function called game
+    Create a loop that loops 5 times
+    Prompt the user for their input
+    call computerPlay
+    call playRound
+    Display the result of the round
+    At the end of the round declare who wins the game
 */
