4d3cf247e2242568d3a3dcdd631a86b495e4993a
timm2 <timlmoran424@gmail.com>
2022-05-30 18:15:16 -0500

    Implemented computerPlay

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ Create a function called game
     Display the result of the round
     At the end of the round declare who wins the game
 */
+
+function computerPlay() {
+    let x = Math.floor(Math.random()*3)+1;
+    if (x == 1){
+        return "rock";
+    }else if (x == 2){
+        return "paper";
+    }else if (x == 3){
+        return "scissors";
+    }
+}
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
