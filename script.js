109adfe554153ea9241234de45c5da6643fabb27
timm2 <timlmoran424@gmail.com>
2022-06-08 13:42:32 -0500

    Added Button functionality and DOM methods

--- script.js
+++ script.js
@@ -38,66 +38,77 @@ function playRound(playerSelection, computerSelection){
     }
     if (lowSelect == "rock"){
         if (computerSelection == "paper"){
+            ++compWins;
             return "You Lose! Paper beats Rock";
         }else if (computerSelection == "scissors") {
+            ++playerWins;
             return "You Win! Rock beats Scissors";
         }
     }
     if (lowSelect == "paper"){
         if (computerSelection == "scissors"){
+            ++compWins;
             return "You Lose! Scissors beats Paper";
         }else if (computerSelection == "rock") {
+            ++playerWins;
             return "You Win! Paper beats Rock";
         }
     }
     if (lowSelect == "scissors"){
         if (computerSelection == "rock"){
+            ++compWins;
             return "You Lose! Rock beats Scissors";
         }else if (computerSelection == "paper") {
+            ++playerWins;
             return "You Win! Scissors beats Paper";
         }
     }
 }
-function game(){
-    let playerWins = 0;
-    let compWins = 0;
-    /*for (let i = 0; i < 5; ++i){
-        let playerSelection = prompt("Enter your choice");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
-        if (result.search("Draw") != -1){
-            continue;
-        }
-        else if (result.search("Win") == -1){
-            ++compWins;
-        }else{
-            ++playerWins;
-        }
-    }*/
-    if (playerWins > compWins){
-        console.log("You Win!");
-    }else if (playerWins < compWins){
-        console.log("You Lose!");
-    }else{
-        console.log("It's a Draw!")
+let playerWins = 0;
+let compWins = 0;
+//Displays Score
+const playerScore = document.querySelector(".player-score");
+playerScore.textContent = playerWins;
+const compScore = document.querySelector(".computer-score");
+compScore.textContent = compWins;
+
+function game(playerChoice, computer){
+    const result = document.querySelector('.result');
+    result.textContent = playRound(playerChoice, computer);
+    playerScore.textContent = playerWins;
+    compScore.textContent = compWins;
+    if(playerWins == 5){
+        result.textContent = "Game Over! You Win";
+    }else if(compWins == 5){
+        result.textContent = "Game Over! You Lose";
     }
 }
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
-    const computer = computerPlay();
-    console.log(playRound('rock', computer));
+    if(playerWins < 5 && compWins < 5){
+        const computer = computerPlay();
+        game('rock', computer)
+    }
 });
-
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
-    const computer = computerPlay();
-    console.log(playRound('paper', computer));
+    if(playerWins < 5 && compWins < 5){
+        const computer = computerPlay();
+        game('paper', computer);
+    }
 });
-
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
-    const computer = computerPlay();
-    console.log(playRound('scissors', computer));
+    if(playerWins < 5 && compWins < 5){
+        const computer = computerPlay();
+        game('scissors', computer);
+    }
 });
-game();
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    playerWins = 0;
+    compWins = 0;
+    playerScore.textContent = playerWins;
+    compScore.textContent = compWins;
+});
+//game();
